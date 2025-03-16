18ca9fb3cff81ce4c790071becdbbfa332d52174
Yosorable <1354464666@qq.com>
2025-03-17 01:12:37 +0800

    change comment

--- minip/AppDelegate.swift
+++ minip/AppDelegate.swift
@@ -9,13 +9,13 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    // MARK: Scene
+    // MARK: scene
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    // MARK: App Delegate
+    // MARK: other lifecycle
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         _ = MWebViewPool.shared

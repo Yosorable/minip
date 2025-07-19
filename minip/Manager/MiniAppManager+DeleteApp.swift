e8310dda97b0e88f7091073d02ad2c14b180a5e0
lzyable <yosora2016@gmail.com>
2025-07-19 15:41:51 +0800

    trash empty app data folder when deleting app

--- minip/Manager/MiniAppManager+DeleteApp.swift
+++ minip/Manager/MiniAppManager+DeleteApp.swift
@@ -11,12 +11,17 @@ extension MiniAppManager {
     func deleteMiniApp(app: AppInfo, completion: () -> Void) {
         let fileManager = FileManager.default
         let appFolder = Global.shared.documentsRootURL.appendingPolyfill(path: app.name)
+        let dataFolder = Global.shared.projectDataFolderURL.appendingPolyfill(path: app.appId)
         do {
             try fileManager.trashItem(at: appFolder, resultingItemURL: nil)
             let db = KVStorageManager.shared.getPrivacyDB()
             for per in MiniAppPermissionTypes.allCases {
                 let key = app.appId + "-" + per.rawValue
                 try db?.deleteValue(forKey: key)
+            }
+
+            if fileManager.fileExists(atPath: dataFolder.path), try fileManager.contentsOfDirectory(atPath: dataFolder.path).isEmpty {
+                try fileManager.trashItem(at: dataFolder, resultingItemURL: nil)
             }
         } catch {
             logger.error("[deleteMiniApp] error: \(error.localizedDescription)")
